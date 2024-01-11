6c355238889792d47e5cc500265ff3b1a01893ab
Andreas <andreaskleanthous2004@gmail.com>
2024-01-11 15:23:19 +0200

    fixed footer on checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 export default async function CheckoutPage() {
 
   return (
-    <div  className="bg-white h-screen">
+    <div  className="bg-white">
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
         <h1 className='font-bold text-3xl text-primary pt-7 pb-2 border-b-2 border-purple-900'>Checkout Page</h1>
         <CheckoutForm />
