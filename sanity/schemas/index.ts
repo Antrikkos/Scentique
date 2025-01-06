b64e57d5a637c7869f8b5284871204bf74d1e0dc
Andreas <andreaskleanthous2004@gmail.com>
2025-01-06 16:56:40 +0200

    Added Boxes to sanity UI

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -3,6 +3,7 @@ import scents from "./scents";
 import heroImages from "./heroImages";
 import product from "./product";
 import weights from "./weights";
-import aboutImages from './aboutImages'
+import aboutImages from './aboutImages';
+import box from './box';
 
 export const schemaTypes = [heroImages, product, category, scents, weights, aboutImages]
